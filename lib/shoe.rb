26953d7508626088c0e0bc564e7a24830bdc0d10
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 22:25:17 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :material, :color, :brand, :condition, :size
-  attr_reader :cobble
+  attr_accessor :material, :color, :condition, :size
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
